3c1b377e5ab8a538456e220afbb6cfeae023e6cf
AliZulnoor <90544813+AliZulnoor@users.noreply.github.com>
2025-05-24 17:06:04 +0300

    Full final push of Silkfy 3D portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from './components/About';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import React from 'react';
 
 const App = () => {
   return (
